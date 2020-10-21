17d6b1ad95654e2e5c9721e153d621d49435de2f
grapeofwrath <grapeofwrath@hey.com>
2020-10-21 13:08:17 -0700

    added alias creation

--- git-update.sh
+++ git-update.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/bash
 
-gitdir=$HOME/GitHub/
+gitdir="$HOME/GitHub/"
 name=($(ls $gitdir))
+aliasn="gitupdate"
+aliasf="$HOME/.bash_aliases"
+alias="alias $aliasn='bash ~/.scripts/git-Update/git-update.sh'"
+
+if grep -q $aliasn $aliasf; then
+     echo "\e[01;32m ==> Starting git-Update...\e[0m";
+else
+    while true; do
+        read -p "Do you want to add gitupdate alias? [y/n]" yn
+        case $yn in
+            [Yy]* ) if [ -f "$aliasf" ]; then
+                    printf "\n# Update GitHub repos\n$alias" >> $aliasf; echo "Alias "$aliasn" added"; break;
+                else
+                    printf "# Update GitHub repos\n$alias" >> $aliasf; echo "Created $aliasf"; echo "Alias "$aliasn" added to $aliasf";
+                fi
+                break;;
+            [Nn]* ) echo "Alias "$aliasn" not added"; echo "\e[01;32m ==> Starting git-Update...\e[0m"; break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
 
 for n in ${name[@]}
 do
     dir=$gitdir$n
     local=$(git -C $dir rev-list --max-count=1 master)
     global=$(git -C $dir rev-list --max-count=1 origin/master)
+    git -C $dir remote update;
     if [ "$local" != "$global" ]; then
         while true; do
             read -p "Do you want to update $n? [y/n]" yn
